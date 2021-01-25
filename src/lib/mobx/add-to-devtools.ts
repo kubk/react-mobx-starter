b6b09e20d1136586b5bf2e08b259e42f3e8824b7
kubk <ya.7gorbachev2016@yandex.ru>
2021-01-25 12:41:44 +0300

    Exclude remotedev from production build

--- src/lib/mobx/add-to-devtools.ts
+++ src/lib/mobx/add-to-devtools.ts
@@ -1,5 +1,7 @@
 const spy = require('mobx-remotedev/lib/spy').default;
 
 export const addToDevtools = (store: object) => {
-  spy(store, { global: true, name: store.constructor.name });
+  if (process.env.NODE_ENV !== 'production') {
+    spy(store, { global: true, name: store.constructor.name });
+  }
 };
